26ce1e5a85cb8c9ccd52530c022d7644c2e3bed1
CrowdHailer <peterhsaxton@gmail.com>
2015-11-12 07:10:01 +0000

    failing calculator test

--- test/main.js
+++ test/main.js
@@ -1,5 +1,9 @@
-describe("A suite", function() {
-  it("contains spec with an expectation", function() {
-    expect(true).toBe(true);
+/*jshint esnext: true */
+
+import * as calculator from "../src/calculator";
+
+describe("A Calculator", function() {
+  it("should be able to add two numbers", function() {
+    expect(calculator.add(1, 2)).toEqual(3);
   });
 });
